f17827bcd830c603da6803aff261cdebad42b540
S1ush <37140143+S1ush@users.noreply.github.com>
2020-10-04 00:23:34 +0530

    route api

--- config/db.js
+++ config/db.js
@@ -2,26 +2,29 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-// synchronous method 
-mongoose.connect(db, {
-    useNewUrlParser: true
-});
+// // synchronous method 
+// mongoose.connect(db, { useNewUrlParser: true });
 
 // Asychromous mehthod which is synchrous just to get error more profoundly
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(db)
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
         console.log('..Mongoose Connected')
 
     } catch (err) {
         console.error(err)
 
         // exit process on failure
+
         process.exit(1)
     }
 };
 
 
 
-module.exports = connectDb
+module.exports = connectDb;
