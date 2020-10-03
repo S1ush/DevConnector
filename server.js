bdf2f49670afd32052e723cbb94b6627ebf2e8d3
S1ush <37140143+S1ush@users.noreply.github.com>
2020-10-04 03:05:41 +0530

    Routing api

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ connectDb();
 
 app.get("/", (req, res) => res.send("API Running "));
 
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running at PORT : ${PORT}`));
