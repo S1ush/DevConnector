fa2091300ddb1ac782362dba367de84e974faf27
Yash Bhole <37140143+S1ush@users.noreply.github.com>
2021-07-14 11:36:50 +0530

    delete experince from user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,4 +145,130 @@ router.get("/user/:user_id", async (req, res) => {
 	}
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile,user & post
+// @access  private
+
+router.delete("/", auth, async (req, res) => {
+	try {
+		await Profile.findOneAndRemove({ user: req.user.id });
+		await User.findOneAndRemove({ _id: req.user.id });
+		res.json({ msg: "User Removed" });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   PUT api/profile
+// @desc    Add profile experience
+// @access  private
+
+router.put(
+	"/experience",
+	[
+		auth,
+		check("title", "Title is required").not().isEmpty(),
+		check("company", "Company is required").not().isEmpty(),
+		check("from", "form is required").not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = await validationResult(req);
+		if (!errors.isEmpty())
+			return res.status(400).json({ error: errors.array() });
+		const { title, company, location, from, to, current, description } =
+			req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			// we can use push here but it will push the data at the end of the object
+			// we use unshift to add data begining to th object
+			profile.experience.unshift(newExp);
+			await profile.save();
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
+// @route   DELETE api/profile
+// @desc    remove experience from profile
+// @access  private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+		//  get remove index
+		// using map(Iterate) to get id in experience array
+		const removeIndex = profile.experience
+			.map((item) => item.id)
+			.indexOf(req.params.exp_id);
+		profile.experience.splice(removeIndex, 1);
+
+		await profile.save();
+		res.send(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json("Server Error");
+	}
+});
 module.exports = router;
+
+// @route    PUT api/profile/education
+// @desc     Add profile education
+// @access   Private
+router.put(
+	"/education",
+	[
+		auth,
+		[
+			check("school", "School is required").not().isEmpty(),
+			check("degree", "Degree is required").not().isEmpty(),
+			check("fieldofstudy", "Field of study is required").not().isEmpty(),
+			check("from", "From date is required").not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { school, degree, fieldofstudy, from, to, current, description } =
+			req.body;
+
+		const newEdu = {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.education.unshift(newEdu);
+
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
